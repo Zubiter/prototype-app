ca40905f628199c97d1b11fd96d5d6862820c6fa
Bill <flyinglimao@aol.com>
2021-02-05 18:21:32 +0800

    Fix: Update create page help text and add some fields

--- src/Create/index.jsx
+++ src/Create/index.jsx
@@ -4,6 +4,7 @@ import {
   Container,
   Form,
   Button,
+  InputGroup,
 } from 'react-bootstrap';
 
 import './Create.css';
@@ -26,7 +27,7 @@ export default class Create extends React.Component {
         <h2 className="page-title">Create a New Collection</h2>
         <section className={this.state.step === 1 ? '' : 'd-none'}>
           <h3>Step 1. Setup Netlify</h3>
-          <small className="text-muted page-description">Assets for NFTs will be upload to netlify, you can upload them to IPFS via Pinata later.</small>
+          <small className="text-muted page-description">Assets for NFTs will be upload to netlify, you can upload them to IPFS with Pinata in Setting.</small>
           <Form>
             <Form.Group controlId="netlify-access-token">
               <Form.Label>Netlify Access Token</Form.Label>
@@ -48,11 +49,27 @@ export default class Create extends React.Component {
             <Form.Group controlId="collection-name">
               <Form.Label>Collection Name</Form.Label>
               <Form.Control type="text" placeholder="Enter Collection Name, such as &quot;My Awesome NFTs&quot;" required />
-              <Form.Text className="text-muted">You can obtain it from <a href="https://app.netlify.com/user/applications" target="_blank" rel="noreferrer">here</a>. Check out <a href="#doc">document</a> if you need detailed instructions.</Form.Text>
             </Form.Group>
-            <Form.Group controlId="collection-name">
+            <Form.Group controlId="collection-symbol">
               <Form.Label>Collection Symbol</Form.Label>
               <Form.Control type="text" placeholder="Enter Collection Symbol, such as &quot;MAN&quot;" required />
+            </Form.Group>
+            <Form.Group controlId="collection-description">
+              <Form.Label>Collection Description</Form.Label>
+              <Form.Control as="textarea" placeholder="(Optional) Enter Collection Description" />
+            </Form.Group>
+            <Form.Group controlId="collection-external-link">
+              <Form.Label>Collection Link</Form.Label>
+              <Form.Control type="text" placeholder="(Optional) Enter Collection Link" />
+            </Form.Group>
+            <Form.Group controlId="collection-image">
+              <Form.Label>Collection Image</Form.Label>
+              <InputGroup>
+                <Form.Control type="text" placeholder="(Optional) Enter Collection Image URL or Upload File" />
+                <InputGroup.Append>
+                  <Button variant="outline-secondary">Select File</Button>
+                </InputGroup.Append>
+              </InputGroup>
             </Form.Group>
             <Button variant="primary" type="button">
               Submit
